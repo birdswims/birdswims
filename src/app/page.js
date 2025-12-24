b7727a24ef4324ca862c9073843a38ab3edf2485
Abhiti Darbar <darbarabhiti@gmail.com>
2025-12-24 19:11:58 +0530

    updated icons

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-jura)]">
+    <div className="flex flex-col items-center justify-center min-h-screen sm:p-20 font-[family-name:var(--font-jura)]">
       <main className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-start justify-center">
-          <div className="text-9xl">BirdSwims</div>
+        <div className="flex flex-row items-center justify-center">
+          <Image
+            src="/Light-transparent.png"
+            width={140}
+            height={140}
+            alt="birdswims logo"
+          />
+          <div className="text-9xl">Birdswims</div>
         </div>
 
-        <div className="mt-4">Coming Soon</div>
+        <div className="">hi @birdswims.com</div>
       </main>
     </div>
   );
